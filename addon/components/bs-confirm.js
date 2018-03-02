8f911e7a9d8d0b622ef1c14f63cf743e6ccebd11
Parker Morse <pmorse@dmgroupk12.com>
2018-03-02 13:40:36 -0500

    🙍 comma

--- addon/components/bs-confirm.js
+++ addon/components/bs-confirm.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import layout from '../templates/components/some-component';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
   classNames: ['modal', 'modal-warning'],
   noActionText: 'Cancel',
   yesActionText: 'Ok',
